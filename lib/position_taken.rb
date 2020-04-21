1bb1a9a0496abaf67bfe9180768db921fe8b0b21
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-21 14:44:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,3 @@
-# code your #position_taken? method here!
-
-board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
-index = 0
-
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
@@ -10,5 +5,3 @@ def position_taken?(board, index)
     true
   end
 end
-
-puts position_taken?(board, index)
