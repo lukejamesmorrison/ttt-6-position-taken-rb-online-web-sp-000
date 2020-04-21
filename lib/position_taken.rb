3846a4b6fccc53405e3d27b0191e98defefc349f
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-21 14:39:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 
 def position_taken?(board, index)
-  !board[index] == " "
+  board[index] != " " || board[index] != nil
 end
 
 puts position_taken?(board, index)
